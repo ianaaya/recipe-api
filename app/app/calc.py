c7c36c5846f84d112af375dfe0c827f3f9c1a094
ianaaya11 <ianaaya81@gmail.com>
2022-08-21 03:37:10 -0400

    Configured docker compose and main to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,8 @@
 def add(x, y):
     return x + y
 
-
 def subtract(x, y):
     return y - x
+
+def quotient(x, y):
+    return x/y
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,11 +3,16 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
-    def test_add_numbers(self):
-        result = calc.add(7,9)
-        self.assertEqual(result, 16)
 
+
+    def test_add_numbers(self):
+        result = calc.add(7, 9)
+        self.assertEqual(result, 16)
 
     def test_subtract_numbers(self):
         result = calc.subtract(4, 15)
         self.assertEqual(result, 11)
+
+    def test_quotient_numbers(self):
+        result = calc.quotient(3, 1)
+        self.assertEqual(result, 3)
